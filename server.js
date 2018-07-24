7790ec6a1684c75ff0fca20b98dbc4101d3a8752
helsey33 <helseysin827@gmail.com>
2018-07-24 20:28:34 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-//Body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB Config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//Connect to mongoDB
+// Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB connected"))
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-//Passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-//Passport Config
+// Passport Config
 require("./config/passport")(passport);
 
-//Use Routes
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-//Serve static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  //set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
@@ -47,6 +44,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
